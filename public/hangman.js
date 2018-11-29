5b8055c46d9dc89a58e9769545a3ee1edd6cba51
Glitch (hello-express) <none>
2018-11-29 20:49:59 +0000

    👯📫 Checkpoint ./public/hangman.js:2204290/657 ./spec/hangman.spec.js:2204290/421

--- public/hangman.js
+++ public/hangman.js
@@ -1,14 +1,12 @@
 function Hangman() {
-this.word = this.chooseWord();
-this.correctLetters = new Set([]);
-this.incorrectLetters = new Set([]);
-this.guessesRemaining = 6;
-this.hasWon = false;
-this.gameOver = false;
+  this.word = this.chooseWord();
+  this.correctLetters = new Set([]);
+  this.incorrectLetters = new Set([]);
+  this.guessesRemaining = 6;
+  this.hasWon = false;
+  this.gameOver = false;
 }
-
 Hangman.prototype.chooseWord = function chooseWord(){
-  
+  fetch("http://app.linkedin-reach.io/words");
 }
-
 module.exports = Hangman;
--- spec/hangman.spec.js
+++ spec/hangman.spec.js
@@ -1,3 +1,4 @@
+/* globals spyOn */
 const Hangman = require('../public/hangman.js');
 let hangman = null;
 describe("hangman", () => {
@@ -6,5 +7,10 @@ describe("hangman", () => {
   });
   it("exists as an object", () => {
     expect(typeof hangman).toBe("object");
+  });
+  
+  it("fetches from the correct endpoint", ()=> {
+    const fetchSpy = spyOn(window, "fetch");
+    expect(fetchSpy);
   });
 });
