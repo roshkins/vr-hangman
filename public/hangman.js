d59cde238280012b5f8b6d47d94c0fade5a9a8c2
Glitch (hello-express) <none>
2018-11-29 20:39:03 +0000

    💄🌯 Checkpoint ./public/hangman.js:2204290/181 ./spec/hangman.spec.js:2204290/876

--- public/hangman.js
+++ public/hangman.js
@@ -1,7 +1,14 @@
 function Hangman() {
 this.word = this.chooseWord();
+this.correctLetters = new Set([]);
+this.incorrectLetters = new Set([]);
+this.guessesRemaining = 6;
+this.hasWon = false;
+this.gameOver = false;
 }
 
-Hangman.prototype.chooseWord = function chooseWord(){}
+Hangman.prototype.chooseWord = function chooseWord(){
+  
+}
 
 module.exports = Hangman;
--- spec/hangman.spec.js
+++ spec/hangman.spec.js
@@ -1,6 +1,10 @@
 const Hangman = require('../public/hangman.js');
 let hangman = null;
 describe("hangman", () => {
-beforeEach(()=>{});
-  it("", () => {});
+  beforeEach(()=>{
+    hangman = new Hangman();
+  });
+  it("exists as an object", () => {
+    expect(typeof hangman).toBe("object");
+  });
 });
