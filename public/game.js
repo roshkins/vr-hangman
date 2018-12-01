41cb76d0abe6092a89f6b0256a48911f785224a0
Glitch (hello-express) <none>
2018-12-01 04:18:53 +0000

    💒🎪 Checkpoint ./views/index.html:2204290/95 ./public/game.js:2204290/240

---
+++ public/game.js
@@ -0,0 +1,6 @@
+/* global Howl */
+var jamaica = new Howl({
+  src: ['https://cdn.glitch.com/9eaf1f46-83c1-4126-8d77-c70ffae16f90%2Fjamaica-girl.mp3?1543539881629'],
+  autoplay: true,
+  loop: true,
+});
