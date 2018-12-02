47d0a7176cea72585cf5eb19c7940410966ff1da
Glitch (hello-express) <none>
2018-12-02 03:48:42 +0000

    💿☔️ Checkpoint ./public/game.js:2204290/243 ./views/index.html:2204290/25

--- public/game.js
+++ public/game.js
@@ -1,7 +1,6 @@
 /* global Howl Hangman annyang*/
 const jamaica = new Howl({
   src: ['https://cdn.glitch.com/9eaf1f46-83c1-4126-8d77-c70ffae16f90%2Fjamaica-girl.mp3?1543539881629'],
-  autoplay: true,
   loop: true,
 });
 
@@ -42,8 +41,14 @@ function processGuess(letter) {
   game.next(letter);
 }
 
+function play() {
+
+}
+
 if (typeof annyang !== "undefined") {
   const commands = {
+    "play": () => jamaica.play(),
+    "music": () => jamaica.play(),
     "mute": () => jamaica.mute(true),
     "stop": () => jamaica.mute(true),
     "shh": () => jamaica.mute(true),
